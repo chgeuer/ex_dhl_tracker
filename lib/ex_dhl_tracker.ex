595aa167246ab5d26888317d6e0f5c7777bc638b
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2024-10-18 17:07:47 +0200

    .

---
+++ lib/ex_dhl_tracker.ex
@@ -0,0 +1,18 @@
+defmodule ExDhlTracker do
+  @moduledoc """
+  Documentation for `ExDhlTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDhlTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_dhl_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDhlTrackerTest do
+  use ExUnit.Case
+  doctest ExDhlTracker
+
+  test "greets the world" do
+    assert ExDhlTracker.hello() == :world
+  end
+end
